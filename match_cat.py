d0bef8789c6923bb1186533d657dfe2d2ad37340
Edward Knueppel <edward.knueppel@zywave.com>
2022-03-10 15:52:47 -0600

    Fixes match_cat

--- match_cat.py
+++ match_cat.py
@@ -1,14 +1,16 @@
 import re
 
 def match_cat(string):
-    if re.search(r'^[Cc][Aa][Tt]$', string):
+    if re.match(r'^[Cc][Aa][Tt]', string):
         return True
     else:
         return False
 
 # For testing
-# print(match_cat('cat'))
-# print(match_cat('CAT'))
-# print(match_cat('Cat'))
-# print(match_cat('cAt'))
-# print(match_cat('dog'))
+print(match_cat('cAt'))
+print(match_cat('cat'))
+print(match_cat('Cat'))
+print(match_cat('CAT'))
+print(match_cat('dog'))
+print(match_cat('caT'))
+print(match_cat('Cats'))

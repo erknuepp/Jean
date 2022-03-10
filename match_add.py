4e479b1f4884e1361ead336ea6aa761bcdc9f27e
Edward Knueppel <edward.knueppel@zywave.com>
2022-03-10 14:22:17 -0600

    Fixes match_add

--- match_add.py
+++ match_add.py
@@ -1,12 +1,18 @@
 import re
 
 def match_add(string):
-    if re.search(r'Add: [1-9] [\+] [1-9]{2}$', string):
-        return True
+    if re.search(r'Add: [0-9]{1} [\+] [1-9]{2}$', string):
+        return string
     else:
-        return False
+        return "This string does not match."
 
-print(match_add('Add: 1 + 23'))
-print(match_add('Add: 2 + 259'))
-print(match_add('Add: 21 + 23'))
-print(match_add('Add: 1 + 23'))
+# For testing uncomment code
+# print(match_add('Add: 1 + 23'))
+# print(match_add('Add: 9 + 51'))
+# print(match_add('Add: 0 + 89'))
+# print(match_add('Add: 4 + 5'))
+# print(match_add('Add: 4 + 5'))
+# print(match_add('Add: 67 + 89'))
+# print(match_add('Add: 1 + 234'))
+# print(match_add('Add: 1 - 23'))
+# print(match_add('Add: 3 + 678'))

aa7583c7b4ba82bcee204dce5250d962082e2a31
Edward Knueppel <edward.knueppel@zywave.com>
2022-03-16 22:44:26 -0500

    get_year_counts

--- get_year_counts.py
+++ get_year_counts.py
@@ -3,7 +3,19 @@ import re
 
 def get_year_counts(lines):
     year_dict = dict()
-    
+    i:str
+    for i in lines:
+        dates = i.split(' ')
+        for d in dates:
+            year = re.split(r'[/|-]', d)[2]
+            if year_dict.get(year) == None:
+                year_dict.setdefault(year, 1)
+            else:
+                year_dict[year] = year_dict[year] + 1
+    new_list = [(k, v) for k, v in year_dict.items()]
+    new_list.sort(key=lambda y: y[1])
+    return new_list
+            
 
 lines = ["02/04/20 12-30-19", "03/04/20", "08-20-18"]
 print(get_year_counts(lines))
